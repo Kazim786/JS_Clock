16f6f725b22fc349575d927f1b40341b26c9ebbc
Kazim Shabbir <Kazimshabbir2010@hotmail.com>
2020-06-02 01:29:20 -0500

    debugged error

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ function setDate(){
     const now = new Date()
     const seconds = now.getSeconds();
     const secondDegrees = ((seconds/60)*360) + 90;
-    secondhand.style.transform = `rotate(${secondsDegrees}deg)`
+    secondhand.style.transform = `rotate(${secondDegrees}deg)`
 }
 
 setInterval(setDate, 1000);

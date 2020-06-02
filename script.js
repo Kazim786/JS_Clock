2e0dce220569e8c281bc93b85d68a0f07518580f
Kazim Shabbir <Kazimshabbir2010@hotmail.com>
2020-06-02 01:34:46 -0500

    clocked completed

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
 const secondhand = document.querySelector('.second-hand')
+const minHand = document.querySelector('.min-hand')
+const hourHand = document.querySelector('.hour-hand')
 
 //video paused at 7:47
 
@@ -7,6 +9,15 @@ function setDate(){
     const seconds = now.getSeconds();
     const secondDegrees = ((seconds/60)*360) + 90;
     secondhand.style.transform = `rotate(${secondDegrees}deg)`
+
+    const minutes = now.getMinutes();
+    const minuteDegrees = ((minutes/60) * 360) + 90
+    minHand.style.transform = `rotate(${minuteDegrees}deg)`
+
+    const hours = now.getHours();
+    const hourDegrees = ((hours/24) * 360) + 90
+    hourHand.style.transform = `rotate(${hourDegrees}deg)`
+
 }
 
 setInterval(setDate, 1000);
